757d2268b26cb5a44d864b94f83d4ad56e12f15b
Ravindra Valand <ravindra@linearloop.io>
2024-09-03 17:37:29 +0530

    freecode

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error('Failed to find the root element');
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
